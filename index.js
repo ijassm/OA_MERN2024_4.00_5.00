9c0145a96b840cafe78ebca8f51e70ee1438a531
“ijassm” <ijass.ocean@gmail.com>
2024-05-31 16:56:38 +0530

    Added : completed recursion

---
+++ index.js
@@ -0,0 +1,30 @@
+// function sum() {
+//   let output = 0;
+
+//   for (let i = 0; i < arguments.length; i++) {
+//     output += arguments[i];
+//   }
+//   return output;
+// }
+
+// console.log(sum(1, 5));
+
+// console.log(sum(1, 5, 10));
+
+// function recursion(n) {
+//   if (n === 0) return n;
+//   recursion(n - 1);
+//   console.log(n);
+// }
+
+// recursion(10);
+
+// const obj = {
+//   name: "xyz",
+//   age: 20,
+//   getName() {
+//     console.log(this);
+//   },
+// };
+
+// obj.getName();
