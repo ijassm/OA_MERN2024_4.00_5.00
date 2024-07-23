6187fbb37d5b89bbf26bdbe7c3c1011725221aeb
“ijassm” <ijass.ocean@gmail.com>
2024-07-23 17:17:47 +0530

    Completed http methods using express

--- Express/index.js
+++ Express/index.js
@@ -2,12 +2,26 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Middleware to parse JSON request bodies
+
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log("middleware called");
+  // res.send("closed");
+  next();
+});
+
 app.get("", (req, res) => {
   res.send("Welcome to express");
 });
 
 app.get("/home", (req, res) => {
   res.send("Welcome to Home");
+});
+
+app.post("/student", (req, res) => {
+  console.log(req.body);
+  res.send("Student added successfully");
 });
 
 app.listen(port, () => {
