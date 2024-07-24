afc41431cea4bb285bfdaa5015e7bcd4bdcb5d25
“ijassm” <ijass.ocean@gmail.com>
2024-07-24 17:29:17 +0530

    Completed create and get method and creating json file

--- Express/index.js
+++ Express/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 3000;
+
+// const students = [];
 
 // Middleware to parse JSON request bodies
 
@@ -15,13 +18,43 @@ app.get("", (req, res) => {
   res.send("Welcome to express");
 });
 
-app.get("/home", (req, res) => {
-  res.send("Welcome to Home");
+app.get("/student", (req, res) => {
+  const students = JSON.parse(fs.readFileSync("student.json", "utf8"));
+  try {
+    if (students.length > 0) {
+      res.send({
+        status: "success",
+        message: "Fetched students",
+        data: students,
+      });
+    } else {
+      res
+        .status(200)
+        .send({ status: "error", message: "No students found", data: null });
+    }
+  } catch (error) {
+    console.log("Error", error.message);
+    res.status(500).send({ status: "error", message: error.message });
+  }
 });
 
 app.post("/student", (req, res) => {
-  console.log(req.body);
-  res.send("Student added successfully");
+  try {
+    const students = fs.readFileSync("student.json", "utf8");
+    if (students === "") {
+      fs.writeFile("student.json", JSON.stringify([req.body]), () => {});
+    } else {
+      fs.writeFile(
+        "student.json",
+        JSON.stringify([...JSON.parse(students), req.body]),
+        () => {}
+      );
+    }
+    res.send({ status: "success", message: "Student added successfully" });
+  } catch (error) {
+    console.log("Error", error.message);
+    res.status(500).send({ status: "error", message: error.message });
+  }
 });
 
 app.listen(port, () => {
