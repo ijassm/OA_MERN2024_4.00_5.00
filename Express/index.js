9a8570a3d468c56b4ece1fdb6d9a7bf74076c0c7
“ijassm” <ijass.ocean@gmail.com>
2024-07-29 17:08:02 +0530

    Completed CRUD operation with file handling

--- Express/index.js
+++ Express/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const uuid = require("uuid");
 const fs = require("fs");
 const app = express();
 const port = 3000;
@@ -18,7 +19,7 @@ app.get("", (req, res) => {
   res.send("Welcome to express");
 });
 
-app.get("/student", (req, res) => {
+app.get("/get-students", (req, res) => {
   const students = JSON.parse(fs.readFileSync("student.json", "utf8"));
   try {
     if (students.length > 0) {
@@ -38,19 +39,83 @@ app.get("/student", (req, res) => {
   }
 });
 
-app.post("/student", (req, res) => {
+app.post("/add-student", (req, res) => {
   try {
     const students = fs.readFileSync("student.json", "utf8");
+    const newObj = { id: uuid.v4(), ...req.body };
     if (students === "") {
-      fs.writeFile("student.json", JSON.stringify([req.body]), () => {});
+      fs.writeFile("student.json", JSON.stringify([newObj]), () => {});
     } else {
       fs.writeFile(
         "student.json",
-        JSON.stringify([...JSON.parse(students), req.body]),
+        JSON.stringify([...JSON.parse(students), newObj]),
         () => {}
       );
     }
     res.send({ status: "success", message: "Student added successfully" });
+  } catch (error) {
+    console.log("Error", error.message);
+    res.status(500).send({ status: "error", message: error.message });
+  }
+});
+
+app.post("/add-students", (req, res) => {
+  try {
+    const students = fs.readFileSync("student.json", "utf8");
+    const listOfStudents = req.body.map((student) => {
+      return { id: uuid.v4(), ...student };
+    });
+    if (students === "") {
+      fs.writeFile("student.json", JSON.stringify(listOfStudents), () => {});
+    } else {
+      fs.writeFile(
+        "student.json",
+        JSON.stringify([...JSON.parse(students), listOfStudents]),
+        () => {}
+      );
+    }
+    res.send({ status: "success", message: "Student added successfully" });
+  } catch (error) {
+    console.log("Error", error.message);
+    res.status(500).send({ status: "error", message: error.message });
+  }
+});
+
+app.delete("/delete-student/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const students = fs.readFileSync("student.json", "utf8");
+    if (students === "") {
+      return res.send({ status: "error", message: "No data found" });
+    } else {
+      const updatedStudents = JSON.parse(students).filter(
+        (student) => student.id !== id
+      );
+      fs.writeFile("student.json", JSON.stringify(updatedStudents), () => {});
+      res.send({ status: "success", message: "Student deleted successfully" });
+    }
+  } catch (error) {
+    console.log("Error", error.message);
+    res.status(500).send({ status: "error", message: error.message });
+  }
+});
+
+app.put("/update-student/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const students = fs.readFileSync("student.json", "utf8");
+    if (students === "") {
+      return res.send({ status: "error", message: "No data found" });
+    } else {
+      const updatedStudents = JSON.parse(students).map((student) => {
+        if (student.id === id) {
+          return { ...student, ...req.body };
+        }
+        return student;
+      });
+      fs.writeFile("student.json", JSON.stringify(updatedStudents), () => {});
+      res.send({ status: "success", message: "Student updated successfully" });
+    }
   } catch (error) {
     console.log("Error", error.message);
     res.status(500).send({ status: "error", message: error.message });
