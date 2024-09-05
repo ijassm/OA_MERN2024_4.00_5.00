43e353e76de97c45d8b535e44d1d2e8dd67c3197
“ijassm” <ijass.ocean@gmail.com>
2024-09-05 17:56:49 +0530

    Completed displaying data and deleting data

--- React/hooks/src/App.jsx
+++ React/hooks/src/App.jsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
-import './App.css'
-import { data } from "./data"
+import { useState } from "react";
+import "./App.css";
+import { data } from "./data";
 
 function App() {
   return (
     <>
       <CardApp />
     </>
-  )
+  );
 }
 
-
 function CardApp() {
-  return <>
-    <main className='flex gap-12 flex-wrap'>
-      {data.map((data, index) => {
-        if (index % 2 === 0) {
-          return <section key={data.id} className='border-green-800 border-solid border-[1px] p-5 max-w-64'>
-            <h2 className='text-2xl my-5'>{data.title}</h2>
-            <p>{data.description}</p>
-          </section>
-        }
-        return <section key={data.id} className='border-red-800 border-solid border-[1px] p-5 max-w-64'>
-          <h2 className='text-2xl my-5'>{data.title}</h2>
-          <p>{data.description}</p>
-        </section>
-      })}
-    </main>
-  </>
+  const [cardData, setCardData] = useState(data);
+
+  const deleteHandler = (id) => {
+    const newCardDate = cardData.filter(data => data.id != id)
+    console.log("deleteHandler called", id);
+    console.log("filter", newCardDate);
+    setCardData(newCardDate);
+  }
+
+  return (
+    <>
+      <main className="flex gap-12 flex-wrap">
+        {cardData.map((data, index) => {
+          return (
+            <section
+              key={data.id}
+              className={`${index % 2 == 0 ? `border-green-800` : `border-red-800`
+                } border-solid border-[1px] p-5 max-w-64`}
+            >
+              <h2 className="text-2xl my-5">{data.title}</h2>
+              <p>{data.description}</p>
+              <button className="outline px-2 my-2" onClick={deleteHandler.bind(this, data.id)}>Delete</button>
+            </section>
+          );
+        })}
+      </main>
+    </>
+  );
 }
 
 function CounterApp() {
@@ -43,32 +54,37 @@ function CounterApp() {
   // console.log(state);
   // console.log(setState);
 
-
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   // console.log(useState);
 
   // let count = 5;
 
   const increment = () => {
     setCount(count + 1);
-  }
+  };
 
   const decrement = () => {
     setCount(count - 1);
-  }
+  };
 
   const reset = () => {
     setCount(0);
-  }
+  };
 
   return (
     <>
       <h1 onClick={increment}>Count : {count}</h1>
-      <button className='increment' onClick={increment}>increment</button>
-      <button className='reset' onClick={reset}>reset</button>
-      <button className='decrement' onClick={decrement}>decrement</button>
+      <button className="increment" onClick={increment}>
+        increment
+      </button>
+      <button className="reset" onClick={reset}>
+        reset
+      </button>
+      <button className="decrement" onClick={decrement}>
+        decrement
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
